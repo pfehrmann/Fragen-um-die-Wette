51e4283ca01bb99357b7e3316687dc30ae29b7eb
Philipp Fehrmann <philipp-09@live.de>
2017-04-10 08:11:04 +0200

    Implemented MatchMakingService

---
+++ core/src/main/java/de/dhbw/application/MatchMakingService.java
@@ -0,0 +1,30 @@
+package de.dhbw.application;
+
+import java.util.Collection;
+
+import com.sun.istack.internal.NotNull;
+
+import de.dhbw.core.DependecyKnowItAll;
+import de.dhbw.core.Match;
+import de.dhbw.core.Question;
+import de.dhbw.core.User;
+
+/**
+ * Created by phili on 06.04.2017.
+ */
+public class MatchMakingService {
+    private static final int QUESTION_COUNT = 18;
+
+    public Match createMatch(@NotNull User userA, @NotNull User userB) {
+        ensureNoRunningMatch(userA, userB);
+        Collection<Question> questions = DependecyKnowItAll.questionRepository.getRandomQuestions(QUESTION_COUNT);
+        Match match = Match.createNewMatch(userA, userB, questions);
+        return match;
+    }
+
+    private void ensureNoRunningMatch(User userA, User userB) {
+        if(DependecyKnowItAll.matchRepository.existsMatch(userA, userB)) {
+            throw new RuntimeException("Match already exists!");
+        }
+    }
+}
