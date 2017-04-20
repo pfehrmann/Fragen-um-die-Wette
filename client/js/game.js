a423e0d92a0e24327b173efa66cad182c6bc6045
Philipp Fehrmann <philipp-09@live.de>
2017-04-20 14:41:06 +0200

    Added class user

--- client/js/game.js
+++ client/js/game.js
@@ -1,6 +1,7 @@
 var name = "";
-var webAddress = "http://localhost:8081";
+var webAddress = "http://192.168.99.100:18082";
 var restAddress = webAddress + "/rest";
+var user;
 
 function submitName() {
     name = $("#playname")[0].value;
@@ -11,7 +12,15 @@ function submitName() {
 function createUser(userName) {
     url = restAddress + "/user/" + userName;
 
-    $.getJSON(url, function (data) {
-        console.log(data);
+    $.post(url, function (data) {
+        user = new User(data.id, data.matchIds, name);
     })
+}
+
+class User {
+    constructor(id, matchIds, name) {
+        this.id = id;
+        this.matchIds = matchIds;
+        this.name = name;
+    }
 }
