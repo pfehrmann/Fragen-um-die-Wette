5303b1521c92c112fceaa9cc103c2f3a37b8ae63
Philipp Fehrmann <philipp-09@live.de>
2017-04-26 14:26:30 +0200

    Fixes

--- client/js/game.js
+++ client/js/game.js
@@ -5,11 +5,15 @@ $(document).ready(function () {
     // Bind actions
     $("#form-login").submit(login);
     $('#button-logout').click(logout);
-
+    $('#new-match-form').submit(startNewGame);
 });
 
 function startNewGame() {
-
+    opponentId = $('#opponent-name').val();
+    Match.createNew(user.id, opponentId, function (match) {
+        $("#area-matches").append(match.toHTML());
+    });
+    return false;
 }
 
 function login() {
@@ -24,6 +28,7 @@ function login() {
         $('#area-login').addClass('hidden');
         $('#area-logout').removeClass('hidden');
         $('#area-matches').removeClass('hidden');
+        $('#area-new-match').removeClass('hidden');
         newUser.showMatches();
     });
 
--- client/js/match.js
+++ client/js/match.js
@@ -3,16 +3,24 @@
  */
 class Match {
 
-    constructor(id, userA, userB, questionIds, currentQuestionUserA, currentQuestionUserB, ansersUserA, answersUserB, matchFinished) {
+    constructor(id, userA, userB, questionIds, currentQuestionUserA, currentQuestionUserB, answersUserA, answersUserB, matchFinished, whoWon) {
         this.id = id;
         this.userA = userA;
         this.userB = userB;
         this.questionIds = questionIds;
         this.currentQuestionUserA = currentQuestionUserA;
         this.currentQuestionUserB = currentQuestionUserB;
-        this.ansersUserA = ansersUserA;
+        this.answersUserA = answersUserA;
         this.answersUserB = answersUserB;
         this.matchFinished = matchFinished;
+        this.whoWon = whoWon;
+    }
+
+    static createMatchFromJSON(data) {
+        if (data.whoWon == null) {
+            return new Match(data.id, data.userA, data.userB, data.questionIds, data.currentQuestionUserA, data.currentQuestionUserB, data.answersUserA, data.answersUserB, data.matchFinished, null);
+        }
+        return new Match(data.id, data.userA, data.userB, data.questionIds, data.currentQuestionUserA, data.currentQuestionUserB, data.answersUserA, data.answersUserB, data.matchFinished, User.createFromJson(data.whoWon));
     }
 
     static createFromId(id, callback) {
@@ -22,7 +30,7 @@ class Match {
             dataType: "json",
             url: url,
             success: function (data) {
-                let match = new Match(data.id, data.userA, data.userB, data.questionsIds, data.currentQuestionUserA, data.currentQuestionUserB, data.ansersUserA, data.answersUserB, data.matchFinished);
+                let match = Match.createMatchFromJSON(data);
                 callback(match);
             }
         });
@@ -36,7 +44,7 @@ class Match {
             url: url,
             data: data,
             success: function (data) {
-                let match = new Match(data.id, data.userA, data.userB, data.questionIds, data.currentQuestionUserA, data.currentQuestionUserB, data.ansersUserA, data.answersUserB, data.matchFinished);
+                let match = Match.createMatchFromJSON(data);
                 callback(match);
             }
         });
@@ -49,19 +57,44 @@ class Match {
             dataType: "json",
             url: url,
             success: function (data) {
-                let match = new Match(data.id, data.userA, data.userB, data.questionIds, data.currentQuestionUserA, data.currentQuestionUserB, data.ansersUserA, data.answersUserB, data.matchFinished);
+                let match = Match.createMatchFromJSON(data);
                 callback(match);
             }
         });
     }
 
     toHTML() {
+        // Check if match if finished.
+        if (this.matchFinished) {
+            let node = $("#match-over").clone().removeClass("hidden");
+
+            let enemyId;
+            // set the enemy and question
+            if (user.id == this.userA) {
+                enemyId = this.userB;
+            } else {
+                enemyId = this.userA;
+            }
+
+            User.create(enemyId, function (enemy) {
+                node.find("[enemy]").text(enemy.name);
+            });
+
+            if (this.whoWon == null) {
+                node.find("[winner]").text("Unentschieden");
+            } else {
+                node.find("[winner]").text(this.whoWon.name);
+            }
+
+            return node;
+        }
         // clone the match prototype
         let node = $("#match").clone().removeClass("hidden");
 
         let enemyId;
         let questionId;
         let currentMatch = this;
+
         // set the enemy and question
         if (user.id == this.userA) {
             enemyId = this.userB;
@@ -78,16 +111,21 @@ class Match {
         Question.create(questionId, function (question) {
             node.find("[question]").text(question.questionText);
             node.find("[answer1text]").text(question.possibleAnswers[0].text);
-            //node.find("[answer2]").text(question.questionText);
-            //node.find("[answer3]").text(question.questionText);
-            //node.find("[answer4]").text(question.questionText);
+            node.find("[answer2text]").text(question.possibleAnswers[1].text);
+            node.find("[answer3text]").text(question.possibleAnswers[2].text);
+            node.find("[answer4text]").text(question.possibleAnswers[3].text);
 
             node.find("[answer1]").val(question.possibleAnswers[0].id);
+            node.find("[answer2]").val(question.possibleAnswers[1].id);
+            node.find("[answer3]").val(question.possibleAnswers[2].id);
+            node.find("[answer4]").val(question.possibleAnswers[3].id);
         });
 
         node.find("[submit-answer]").submit(function () {
             let answerId = $('[submit-answer] input:radio:checked').val();
-            currentMatch.answerQuestion(user.id, answerId);
+            currentMatch.answerQuestion(user.id, answerId, function (match) {
+                node.replaceWith(match.toHTML())
+            });
             return false;
         });
 
